ce3de30da626bd9d28dc205ef7fdb6c4ec253720
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-12-22 15:17:37 +0200

    feat: Created and configured login and sign up feature,configured firebase connection for ios and android,added email credentials for login and sign up.

---
+++ app/(auth)/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+const Page = () => {
+  const user = auth().currentUser;
+
+  return (
+    <View>
+      <Text>Welcome back back {user?.email}</Text>
+      <Button title="Sign out" onPress={() => auth().signOut()} />
+    </View>
+  );
+};
+export default Page;
